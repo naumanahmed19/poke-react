27602f55106c1f12a5b49b5c757fa799b0c435e2
Nomi <naumanahmed19@gmail.com>
2019-08-25 11:51:58 +0200

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link btn btn-primary"
           href="https://reactjs.org"
           target="_blank"
+
           rel="noopener noreferrer"
         >
           Learn React
