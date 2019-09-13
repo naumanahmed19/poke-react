19cbd7191e9f90530884eefb4d6c71f576afc61e
Nomi <naumanahmed19@gmail.com>
2019-09-13 16:40:17 +0200

    git ignore

---
+++ build/precache-manifest.eca3e008fa4c231ffa4c2768106427cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4631c283ff79a38d383910ffd98e24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "980f35c599710e0e2c87",
+    "url": "/static/css/2.b7c6c5e7.chunk.css"
+  },
+  {
+    "revision": "bbc014c80e0506b77670",
+    "url": "/static/css/main.1f1ecc55.chunk.css"
+  },
+  {
+    "revision": "980f35c599710e0e2c87",
+    "url": "/static/js/2.100f5918.chunk.js"
+  },
+  {
+    "revision": "bbc014c80e0506b77670",
+    "url": "/static/js/main.f10a92be.chunk.js"
+  },
+  {
+    "revision": "ffe253340dbca9a1a9b1",
+    "url": "/static/js/runtime~main.50fc0bd9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
